fd9ddc6317e4cf83d7d491613e5165163d6dba71
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2019-11-29 19:41:40 -0500

    Add structure for steps 2 and 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <regex>
 #include <string>
 #include <utility>
@@ -9,13 +10,17 @@
 
 int main()
 {
-    // First we do the handshake
+    std::regex server_out_regex("^\\w+\\.out$");
+    std::optional<std::ifstream> server_out_file;
+
+    // First: Do the handshake.
 
     size_t token;
-    // std::pair<size_t, size_t> scope;
+    // We assume that the size will always be 10x10.
+    std::pair<size_t, size_t> scope = {10, 10};
 
     // TODO: Probably factorize this to a function returning a size_t (the
-    //       token).
+    //       token)
     size_t try_number = 0;
     while (true)
     {
@@ -23,19 +28,46 @@ int main()
 
         handshake_write(team_name + std::to_string(try_number));
 
-        std::regex file_regex("^\\w+\\.out$");
-        std::optional<std::ifstream> file_in;
         // TODO: Here we wait til we get a response in 'Player.out' or something
-        while (file_in.has_value())
+        while (!server_out_file.has_value())
         {
-            file_in = open_file(file_regex);
+            server_out_file = open_file(server_out_regex);
         }
 
-        auto parts = read(file_in.value());
+        auto parts = read(server_out_file.value());
 
         if (parts["STATUS"] != "ACCEPTED")
         {
             try_number++;
         }
     }
+
+    // Second: Position the fleet
+    std::map<size_t, size_t> remaining_fleet = {
+        {1, 4},
+        {2, 3},
+        {3, 2},
+        {4, 1}
+    };
+
+    //  TODO: Fleet placement.
+
+    // Third: Start attacking
+
+    // Fill all the remaining positions to attack.
+    std::set<std::pair<size_t, size_t>> pos_left_to_shoot;
+    for (size_t i = 0; i < scope.first; i++)
+    {
+        for (size_t j = 0; j < scope.second; j++)
+        {
+            pos_left_to_shoot.emplace(i, j);
+        }
+    }
+
+    while (!pos_left_to_shoot.empty())
+    {
+        auto n_remaining = pos_left_to_shoot.size();
+    }
+
+    server_out_file.reset();
 }
