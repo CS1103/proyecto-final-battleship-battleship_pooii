52ed09a05fc4f851484a7dceab812f09b083a30f
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2019-12-01 01:29:16 -0500

    Make main use the new Player interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,17 @@
-#include <fstream>
 #include <iostream>
-#include <map>
-#include <random>
-#include <regex>
-#include <stack>
-#include <string>
-#include <utility>
 
-#include "funct.hpp"
+#include "Player.hpp"
 #include "prettyprint.hpp"
 
 int main()
 {
-    std::regex server_out_regex("^\\w+\\.out$");
-    std::optional<std::ifstream> server_out_file;
-
-
-    // Second: Position the fleet
-    std::map<size_t, size_t> remaining_fleet = {
-        {1, 4},
-        {2, 3},
-        {3, 2},
-        {4, 1}
-    };
-
-    // TODO: Fleet placement.
-
-    // Third: Start attacking
-
-
+    Player p("TeamA");
+    if (p.play())
+    {
+        std::cout << "You won" << std::endl;
+    }
+    else
+    {
+        std::cout << "You lost" << std::endl;
+    }
 }
